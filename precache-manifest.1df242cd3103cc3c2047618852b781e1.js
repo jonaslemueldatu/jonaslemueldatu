3d11d91f176e7b6256192f969c7e199b0a478a71
Gian Karla Durian <giankarladurian@Gians-MacBook-Pro.local>
2020-08-03 18:21:10 +0800

    Updates

---
+++ precache-manifest.1df242cd3103cc3c2047618852b781e1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7737b45bc89a96f3209d2ca77fbcf90a",
+    "url": "/jonaslemueldatu/index.html"
+  },
+  {
+    "revision": "44b6c7dca17619dfe000",
+    "url": "/jonaslemueldatu/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "107983af348ada31ebe8",
+    "url": "/jonaslemueldatu/static/css/main.6d584463.chunk.css"
+  },
+  {
+    "revision": "44b6c7dca17619dfe000",
+    "url": "/jonaslemueldatu/static/js/2.73a2312a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jonaslemueldatu/static/js/2.73a2312a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107983af348ada31ebe8",
+    "url": "/jonaslemueldatu/static/js/main.2b723fdc.chunk.js"
+  },
+  {
+    "revision": "6989de2504d55c9daff8",
+    "url": "/jonaslemueldatu/static/js/runtime-main.8392b63f.js"
+  },
+  {
+    "revision": "739c533278103a4af6286959898c9f65",
+    "url": "/jonaslemueldatu/static/media/cityBackground.739c5332.jpg"
+  },
+  {
+    "revision": "59eac6319703f8f5ec9df8aca750e970",
+    "url": "/jonaslemueldatu/static/media/gears.59eac631.png"
+  },
+  {
+    "revision": "76dfc0be5f56c3a44bd20714b5faf346",
+    "url": "/jonaslemueldatu/static/media/jonasSablay.76dfc0be.jpg"
+  }
+]);
